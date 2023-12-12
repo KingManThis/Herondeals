959198b7ac339faabf25349f1c233d6c01a8e496
KingManThis <93237645+KingManThis@users.noreply.github.com>
2023-12-12 03:20:45 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,18 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Handle form submission logic here
+    // You can access form data using event.target.elements
+    const firstName = event.target.elements.firstName.value;
+    const email = event.target.elements.email.value;
+
+    // Do something with the form data, e.g., send it to a server
+    console.log('Submitted:', { firstName, email });
+  };
+
   return (
     <div className="container">
       <Head>
@@ -11,13 +23,28 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Herondeals!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Automation at its best <code>Business Solutions</code>
         </p>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
